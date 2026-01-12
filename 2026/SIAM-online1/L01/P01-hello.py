bc11dd833af0a5d337f6f7bbc6287ba524d2453f
Paolo Aliverti <paolo.aliverti@gmail.com>
2026-01-12 19:23:22 +0100

    lezione 1

---
+++ 2026/SIAM-online1/L01/P01-hello.py
@@ -0,0 +1,28 @@
+# Primo programma in python
+# 12/01/2026
+
+print("hello Python") #stampo a video
+print('hello Python 2')
+
+'''
+nel mezzo del cammin
+di nostra vita
+mi ritrovai...
+'''
+
+print("mela", 2, "pera", 12)
+print(10, "più", 20 , "è uguale a", 10+20)
+
+print("mela", ",", 12, ",", "pera")
+
+print("mela", 2, "pera", 12, sep="#")
+print("mela", 2, "pera", 12, sep=",")
+
+print("ciao", end=" ")
+print("mondo")
+
+print("messaggio di avviso numero 123")
+print("messaggio di avviso\nnumero 123")
+print("messaggio di avviso\\nnumero 123")
+
+
