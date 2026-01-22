e8e055c8e0f430f6eda8cbf0d90f892e49559672
Paolo Aliverti <paolo.aliverti@gmail.com>
2026-01-22 09:53:50 +0100

    L07-08

--- 2026/Siam_BigData/L07/P06_mymod.py
+++ 2026/Siam_BigData/L07/P06_mymod.py
@@ -1,4 +1,5 @@
 import re
+import csv
 
 EMAIL_REGEX = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$")
 
@@ -11,3 +12,18 @@ def renameCol(row, old_name, new_name):
     valore = row[old_name]
     row.pop(old_name)
     row[new_name] = valore
+
+def getEtaMedia(nome_file_csv):
+    media_age = 0
+    ages = []
+    with open(nome_file_csv, newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            age = row.get("Age", "").strip()
+            if age.isdigit():
+                ages.append(int(age))
+
+    media_age = sum(ages) / len(ages)
+    media_age = round(media_age)
+
+    return media_age
